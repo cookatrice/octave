6ae3a82bac5cd518874bdbe777b85385180d201e
cookatrice <cookatrice@sk.com>
2019-12-27 13:18:48 +0900

    update lrCostFunction.m

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -38,13 +38,11 @@ grad = zeros(size(theta));
 
 h_theta = sigmoid(X * theta);
 
-J = (1/m) * sum(-y .* log(h_theta) - (1-y) .* log(1-h_theta));
-grad = (1/m) * X' * (h_theta-y);
+J = (1/m)*sum(-y.*log(h_theta)-(1-y).*log(1-h_theta)) + (lambda/(2*m))*sum(theta(2:end).^2);
 
-
-
-
-
+temp = theta;
+temp(1) = 0;
+grad = (1/m) * X' * (h_theta-y) + ((lambda / m) * temp);
 
 % =============================================================
 
